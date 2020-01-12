d0b9303cabbca9d4cf211d77d33ecff01fd25434
Willy <mail@willybrauner.com>
2020-01-12 19:02:43 +0100

    comment

--- config/scaffolder/config.js
+++ config/scaffolder/config.js
@@ -1,13 +1,14 @@
 const appRoot = require("app-root-path");
 
 module.exports = {
+  // bundle path
   bundlePath: appRoot.resolve("src"),
 
   // Skeletons path
   templatesPath: appRoot.resolve("config/scaffolder/templates"),
 
   // Scaffold compatible folder
-  componentCompatibleFolders: ["components", "pages", "views"],
+  componentCompatibleFolders: ["components", "pages"],
 
   // Scaffold bundle type
   bundleType: ["react", "dom"]
