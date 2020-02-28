2a0b36e69a4d166d0454e4e28d4d241a0d83af30
Willy <mail@willybrauner.com>
2020-02-28 14:34:52 +0100

    prebuild-react-page-list as default prebuild task

--- config/tasks/prebuild/index.js
+++ config/tasks/prebuild/index.js
@@ -1,9 +1,6 @@
 const { prebuildReactPagesList } = require("../prebuild-react-pages-list");
 const { prebuildHtaccess } = require("../prebuild-htaccess");
 const { prebuildDotenv } = require("../prebuild-dotenv");
-const {
-  getInstallConfigHelper
-} = require("../../helpers/get-install-config-helper");
 const { prebuildBundleList } = require("../prebuild-bundle-list/index");
 const debug = require("debug")("config:prebuild");
 
@@ -30,6 +27,9 @@ const prebuild = (pEnv = null) => {
     // prebuld bundle list for webpack
     await prebuildBundleList();
 
+    // prebuild react pages list
+    await prebuildReactPagesList();
+
     // prebuild htaccess file
     if (config.prebuildHtaccess) await prebuildHtaccess();
 
@@ -37,16 +37,10 @@ const prebuild = (pEnv = null) => {
     if (config.prebuildDotEnv) await prebuildDotenv(pEnv);
 
     /**
-     * Optional
-     * These brebuild-tasks can be removed and you can add your own.
+     * Custom tasks
+     * Set your custom brebuild-tasks below
+     * ...
      */
-    // get install config content file
-    const getInstallConfig = await getInstallConfigHelper();
-    // if extist and we are on react bundle type
-    if (getInstallConfig !== null && getInstallConfig.bundleType === "react") {
-      debug("this is a react bundleType, prebuild react pages list...");
-      await prebuildReactPagesList();
-    }
 
     // end
     resolve();
