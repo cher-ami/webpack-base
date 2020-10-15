0201018788b11b691d62661ce87df5e473bf4e1c
willybrauner <mail@willybrauner.com>
2020-10-15 17:02:39 +0200

    Init component test. Need to add test on pages

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { defaults } = require("jest-config");
+module.exports = {
+  // ...
+  moduleFileExtensions: [
+    ...defaults.moduleFileExtensions,
+    "js,",
+    "jsx",
+    "ts",
+    "tsx",
+  ],
+  // ...
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/config/jest/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/config/jest/styleMock.js",
+  },
+  moduleDirectories: ["node_modules", "src"],
+};
