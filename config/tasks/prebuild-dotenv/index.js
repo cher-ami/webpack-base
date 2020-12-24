999af66c67f91a8b0db8eaa0e2b269003e4969b9
willybrauner <mail@willybrauner.com>
2020-12-24 11:18:00 +0100

    Update dotenv task to remove ENV var from .env file

--- config/tasks/prebuild-dotenv/index.js
+++ config/tasks/prebuild-dotenv/index.js
@@ -2,79 +2,59 @@ const { Files } = require("@zouloux/files");
 const debug = require("debug")("config:prebuild-dotenv");
 const { logs } = require("../../helpers/logs-helper");
 const config = require("../../global.config");
-const paths = require("../../global.paths");
+const appRoot = require("app-root-path");
 
 /**
  * Prebuild .env file
- * Create and inject .env file in specific folder (dist/ by default)
+ * Create and inject .env file in specific folder
  */
-const prebuildDotEnv = (destinationPath = config.outputPath) => {
+const prebuildDotEnv = (newFilePath = `${config.outputPath}/.env`) => {
   return new Promise((resolve) => {
-    logs.start(`Prebuild .env in ${destinationPath} folder`);
-    debug("process.env.ENV", process.env.ENV);
+    // read all .env files and get all var names
+    const envFiles = Files.getFiles(appRoot.resolve(".env*")).files;
+    debug("available env files", envFiles);
 
-    // env file we want to inject in output path
-    const newFilePath = `${destinationPath}/.env`;
-    // select param env or process env or null
-    let selectedEnv = process.env.ENV || null;
+    // get vars from all .env files and add them to the same array
+    const vars = envFiles
+      .map((el) =>
+        // read current .env file
+        Files.getFiles(el)
+          .read()
+          // split each lines
+          .split("\n")
+          // for each line, filter comments and keep only var name
+          .map((el) => {
+            const isComment = el.includes("#");
+            const isEmptyLine = el === "";
+            const containsEqual = el.includes("=");
+            if (!isEmptyLine && !isComment && containsEqual) {
+              const varName = el.split("=")[0];
+              return varName ? varName : null;
+            }
+          })
+          // remove empty lines
+          .filter((e) => e)
+      )
+      // flat arrays results
+      .flat()
+      // remove double entries
+      .filter((elem, index, self) => index === self.indexOf(elem));
 
-    selectedEnv
-      ? logs.note(`Selected env is ${selectedEnv}`)
-      : logs.note(`There is no selected env, we use content of default .env`);
+    debug("available vars after merge vars from all .env files", vars);
 
-    // env extension depend on currentEnv
-    const envNameExtension = selectedEnv === "development" ? "" : selectedEnv;
-    debug("envNameExtension", envNameExtension);
-
-    const dotEnvFileName = `.env${
-      envNameExtension !== "" ? `.${envNameExtension}` : ""
-    }`;
-
-    logs.note(`dotEnv file used is: ${dotEnvFileName}`);
-
-    // build template file path
-    const templateFilePath = `${paths.root}/${dotEnvFileName}`;
-
-    debug({ envNameExtension, dotEnvFileName, newFilePath, templateFilePath });
-
-    // read .env
-    const envFile = Files.getFiles(templateFilePath).read();
-
-    // prettier-ignore
-    const keepingEnvVarsArray = envFile
-      .split("\n")
-      .map((el) => {
-        const isComment = el.includes("#");
-        const isEmptyLine = el === "";
-        const containsEqual = el.includes("=");
-        if (
-            !isEmptyLine && 
-            !isComment && 
-            containsEqual
-        ) {
-          const varName = el.split("=")[0];
-          return varName ? varName : null;
-        }
-      })
-      .filter((e) => e);
-
+    let template;
     // create template with varNames and process.env values
-    const template = keepingEnvVarsArray
-      .map((el) => `${el}=${process.env[el]}`)
-      .join("\n");
+    template = vars.map((el) => process.env[el] && `${el}=${process.env[el]}`);
 
-    debug("template", template);
+    // push current version in it
+    template.push(`VERSION=${require("../../../package").version}`);
+
+    // filter to remove empty lines
+    template = template.filter((e) => e).join("\n");
+    debug("template to write in file", template);
 
     debug("write .env file...");
     Files.new(newFilePath).write(template);
-
-    logs.note(`write "VERSION=..." in dist/.env file...`);
-    Files.getFiles(newFilePath).alter((fileContent) => {
-      return fileContent.replace(
-        /VERSION=/,
-        `VERSION=${require("../../../package").version}`
-      );
-    });
 
     logs.done();
     resolve();
